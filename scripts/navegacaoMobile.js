9c23548416593ff46eab568903ae7f948b287806
Navarone <brunohenrique.travessa@gmail.com>
2023-07-13 11:59:06 -0300

    .

--- scripts/navegacaoMobile.js
+++ scripts/navegacaoMobile.js
@@ -1,36 +1,40 @@
-class MobileNavbar{
-    constructor(mobileMenu, navList, navLinks){
-        this.mobileMenu = document.querySelector(mobileMenu);
-        this.navList = document.querySelector(navList);
-        this.navLinks = document.querySelectorAll(navLinks);
-        this.activeClass = "active";
-        this.handleClick = this.handleClick.bind();
-    }
+class MobileNavbar {
+  constructor(mobileMenu, navList, navLinks) {
+    this.mobileMenu = document.querySelector(mobileMenu);
+    this.navList = document.querySelector(navList);
+    this.navLinks = document.querySelectorAll(navLinks);
+    this.activeClass = "active";
+    this.handleClick = this.handleClick.bind();
+  }
 
-    animatedLinks(){
-        this.navLinks.forEach((link) => {
-            link.style.animation
-            ?(link.style.animation = "")
-            :(link.style.animattion = "navLinkFade 0.5 ease fowards 0.3s");
-        })
-    }
+  animatedLinks() {
+    this.navLinks.forEach((link) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animattion = "navLinkFade 0.5 ease fowards 0.3s");
+    });
+  }
 
-    handleClick= () => {
-        console.log(this)
-        this.navList.classList.toggle(this.activeClass); 
-    }
+  handleClick = () => {
+    console.log(this);
+    this.navList.classList.toggle(this.activeClass);
+  };
 
-    addClickEvent(){
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
 
-    init(){
-        if(this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
 
-const mobileNavbar = new MobileNavbar(".menu-mobile", ".paginacao", ".paginacao li");
+const mobileNavbar = new MobileNavbar(
+  ".menu-mobile",
+  ".paginacao",
+  ".paginacao li"
+);
 mobileNavbar.init();
